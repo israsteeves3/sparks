cc783bb67f3e796a28d4e7b34da1e278cb9bb63e
rdeditus <robert.deditus@edu.sait.ca>
2026-03-10 10:45:32 -0600

    Uh oh
    I forgot all that i did...

--- versone.js
+++ versone.js
@@ -1,49 +1,8 @@
-function changeValue(){
-    console.log(document.getElementById('sliderRange').value);
-    let v = document.getElementById('sliderRange').value;
-    document.getElementById('cardContainer').style.transform="rotate(" + v + "deg)";
+
+document.getElementById('prev').onclick = function(){
+    document.getElementById('hobbyCard').textContent = "You chose to go to the previous slide";
 }
 
-document.getElementById('sliderRange').addEventListener('input',changeValue);
-
-function changeOpacity(){
-    let v = document.getElementById('sliderRange').value;
-    let s = Math.abs(v);
-    let t = s - 90
-    let o = t/100;
-    let p = Math.abs(o)
-    document.getElementById('cardContainer').style.opacity = p;
+document.getElementById('next').onclick = function(){
+    document.getElementById('hobbyCard').textContent = "You chose to go to the next slide";
 }
-
-document.getElementById('sliderRange').addEventListener('input',changeOpacity);
-
-//This function is to remove the card and bring forth the next card.
-function removeCard(){
-    let v = document.getElementById('sliderRange').value;
-    const card = document.getElementById('cardContainer');
-    if (v <= -45){
-        card.textContent = "hello";
-        document.getElementById('sliderRange').value = 0;
-        document.getElementById('cardContainer').style.transform="rotate(" + 0 + "deg)";
-        document.getElementById('cardContainer').style.opacity = 1;
-        document.getElementById('cardContainer').style.height = "700px";
-    }
-    if (v >= 50){
-        card.textContent = "New Card Information Here!";
-        document.getElementById('sliderRange').value = 0;
-        document.getElementById('cardContainer').style.transform="rotate(" + 0 + "deg)";
-        document.getElementById('cardContainer').style.opacity = 1;
-        document.getElementById('cardContainer').style.height = "700px";
-    }
-    else {
-    }
-}
-
-document.getElementById('sliderRange').addEventListener('input',removeCard);
-
-
-
-//function resetSlider(){
-//   document.getElementById('sliderRange').Value0.reset();
-//}
-
